bc638ad8be5a74e2e26322b96f6708b9f00f6cb7
vidhyanandreddy <vidhyanand067@gmail.com>
2025-07-03 17:23:54 +0530

    Completed Project --> RestAPIs,Postman

---
+++ src/main/java/com/example/student_library_management_system/responsedto/BookResponseDto.java
@@ -0,0 +1,94 @@
+package com.example.student_library_management_system.responsedto;
+
+public class BookResponseDto {
+    private int id;
+    private String title;
+    private String publisherName;
+    private String publishedDate;
+    private int pages;
+    private int price;
+    private boolean availability;
+    private String rackNo;
+    private String category;
+    private String authorName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getRackNo() {
+        return rackNo;
+    }
+
+    public void setRackNo(String rackNo) {
+        this.rackNo = rackNo;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
